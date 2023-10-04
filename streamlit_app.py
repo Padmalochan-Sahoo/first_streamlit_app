45935c6dbce3d881cfcb26cf07a9307cea414f70
Padmalochan-Sahoo <105733502+Padmalochan-Sahoo@users.noreply.github.com>
2023-10-05 00:17:36 +0530

    Update streamlit_app.py
    for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,3 +61,6 @@ streamlit.dataframe(my_data_rows)
 # adding another text for user feedback
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding:', add_my_fruit)
+
+#for test this line 
+my_cur.execute("insert into fruit_load_list values('for streamlit')")
