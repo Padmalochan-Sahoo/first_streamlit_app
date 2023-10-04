9a0bb234fd96f06b556bda9eddb3c8bdce0ea90c
Padmalochan-Sahoo <105733502+Padmalochan-Sahoo@users.noreply.github.com>
2023-10-05 02:18:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ streamlit.dataframe(fruits_to_show)
 # import requests
 
 # New way to display with try expect with nested if else
-streamlit.header("Fruityvice Fruit Advice!")
+"""streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -53,7 +53,28 @@ try:
     fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalize)
 except URError as e:
+  stramlit.error()"""
+
+#  we will move several lines of code into a little group of code called a function.
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalize
+
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please enter a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(this_fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URError as e:
   stramlit.error()
+
+  
+  
 
 
 # streamlit.text(fruityvice_response) --this one just show the line as text forma 
@@ -64,21 +85,57 @@ except URError as e:
 #streamlit.dataframe(fruityvice_normalize) 
 
 # import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone() - it will fetch one row
-# streamlit.text("Hello from Snowflake:") - Header will show like hello from snowflake
-# streamlit.text(my_data_row) - 
-my_data_rows = my_cur.fetchall()
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+''' my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+ my_data_row = my_cur.fetchone() - it will fetch one row
+ streamlit.text("Hello from Snowflake:") - Header will show like hello from snowflake
+ streamlit.text(my_data_row) - '''
+
+
 streamlit.header("The fruit load list contains:")
+
+# Snowflake related function
+def get_fruit_load_least():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+# Add a button to load a fruit
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+  
+# Allow the end user to add a fruit to the list
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values('" + ???? + "')")
+      return "Thanks for adding new Friuit:" + new_fruit
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Get Fruit load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = get_fruityvice_data(add_my_fruit)
+  streamlit.text(back_from_function)
+
+
+      
+
+  
+
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
+
+'''my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 # adding another text for user feedback
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding:', add_my_fruit)
 
 #for test this line 
-my_cur.execute("insert into fruit_load_list values('for streamlit')")
+# my_cur.execute("insert into fruit_load_list values('for streamlit')")
 
-#streamlit.stop()
+#streamlit.stop()'''
