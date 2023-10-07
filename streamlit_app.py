bee7593da585416e34a2bffb6ebc2a72106e1780
Padmalochan-Sahoo <105733502+Padmalochan-Sahoo@users.noreply.github.com>
2023-10-07 12:17:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,10 +87,10 @@ except URError as e:
 # import snowflake.connector
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
-''' my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
- my_data_row = my_cur.fetchone() - it will fetch one row
- streamlit.text("Hello from Snowflake:") - Header will show like hello from snowflake
- streamlit.text(my_data_row) - '''
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+ # my_data_row = my_cur.fetchone() - it will fetch one row
+ # streamlit.text("Hello from Snowflake:") - Header will show like hello from snowflake
+ # streamlit.text(my_data_row) - '''
 
 
 streamlit.header("The fruit load list contains:")
